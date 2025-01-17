7a0bee4c80e5788b6c3ba9a158db8cdf3105a207
jptwagira <70667595+jptwagira@users.noreply.github.com>
2025-01-17 21:37:31 +0000

    Deploying to gh-pages from @ jptwagira/jptwagira.github.io@aae59ccfe6352aeb7176d5358503fa53617e1658 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,7 +492,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%77%61%67%69%72%61%70%65%74%65%72%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -513,14 +513,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=s8XfhM0AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
