21f7d4fa3b2aa20d2a613f98be966d5708557c0b
jptwagira <70667595+jptwagira@users.noreply.github.com>
2025-02-08 14:43:13 +0000

    Deploying to gh-pages from @ jptwagira/jptwagira.github.io@7c13f6b4ae0c7972e0f700c78ebfc60a0c3ceed5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,8 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-",
-          title: "",
+        },{id: "nav-repositories",
+          title: "repositories",
           description: "I am a PhD candidate and a research assistant in the field of experimental astroparticle physics, working on neutrino research spanning data analysis and software development for tools designed for scientific applications.",
           section: "Navigation",
           handler: () => {
