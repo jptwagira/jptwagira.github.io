84f336e168f78cc826f97cb0afe70d2fdb458a38
jptwagira <70667595+jptwagira@users.noreply.github.com>
2025-04-17 14:05:31 +0000

    Deploying to gh-pages from @ jptwagira/jptwagira.github.io@8d2ed7037e4a93a98fae92690387bf3778fd2a4f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,18 +79,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "post-",
-      
-        title: "",
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2025/2024-03-25-productivity/";
-        
-      },
-    },{id: "post-observation-of-an-ultra-high-energy-cosmic-neutrino-with-km3net-nature",
+        },{id: "post-observation-of-an-ultra-high-energy-cosmic-neutrino-with-km3net-nature",
       
         title: 'Observation of an ultra-high-energy cosmic neutrino with KM3NeT | Nature <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
@@ -165,6 +154,17 @@ ninja.data = [{
       handler: () => {
         
           window.open("https://www.coursera.org/articles/writing-skills", "_blank");
+        
+      },
+    },{id: "post-mastering-writing-skills-tips-tools-and-resources",
+      
+        title: "Mastering Writing Skills Tips, Tools, and Resources",
+      
+      description: "How to Improve Writing Skills",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/productivity/";
         
       },
     },{id: "post-a-post-with-code-diff",
