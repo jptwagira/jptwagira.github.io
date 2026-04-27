f479189e830bc27120ec47cfd8b28b963d6fa12b
jptwagira <70667595+jptwagira@users.noreply.github.com>
2026-04-27 23:38:38 +0000

    Deploying to gh-pages from @ jptwagira/jptwagira.github.io@1f2218615cf59c8541921ba4b1796a2bc6240839 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "I am a research assistant in the field of experimental astroparticle physics, working on neutrino research spanning data analysis and software development for tools designed for scientific applications.",
+          description: "I earned my PhD in Physics in spring 2026 and a Master of Science in Physics in 2020, both from Michigan State University. Before joining MSU, I obtained a Master of Science in Mathematical Sciences from the African Institute for Mathematical Sciences in 2018 and Bachelor degree from the University of Rwanda in 2015.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
